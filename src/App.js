9241b56cf691533d60f62882ffac299729ed440b
Mayur Chinche <mayur.chinche@phonepe.com>
2024-11-30 14:35:19 +0530

    added changes for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Register from './component/Register';
 import Login from './component/Login';
 function App() {
   return (
